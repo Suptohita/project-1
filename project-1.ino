e6496c3d0dba652662afb9ccb01904dbc583ab04
Arvind <80598358+Suptohita@users.noreply.github.com>
2025-12-26 17:49:40 +0100

    Update project-1.ino

--- project-1.ino
+++ project-1.ino
@@ -71,7 +71,7 @@ void loop() {
 void setColor(int redValue, int greenValue, int blueValue) {
   analogWrite(redPin, redValue);
   analogWrite(greenPin, greenValue);
-  analogWrite(bluePin, blueValue);a
+  analogWrite(bluePin, blueValue);
 }
 
 void clearSerialBuffer() {
