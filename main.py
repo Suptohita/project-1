75d8b38bd038351a5746546cc16b6a0517910004
Arvind <80598358+Suptohita@users.noreply.github.com>
2026-01-12 04:45:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from machine import I2C, Pin
-# from libs import tcs34725
+from libs import tcs34725
 from libs import rgb_led, button, pot_dimmer
+from libs.tcs34725 import html_rgb
 import time
 
 def sleep(s):
     time.sleep(s)
 
+# sensor
 i2c = I2C(0, scl=Pin(22), sda=Pin(21))
-# sensor's led
-Pin(17, Pin.OUT).value(0)
+Pin(19, Pin.OUT).value(0)
+sensor = tcs34725.TCS34725(i2c)
+# sensor.gain(5)
 
-# sensor = tcs34725.TCS34725(i2c)
-rgb = rgb_led.RGBLED(17, 5, 16)
 lock_btn = button.Button(23)
-target_color = rgb.generate_random_color(True)
 
-print('Generating Color...')
-sleep(10)
-print(f'Your Target color: {target_color}')
+# rgb led
+rgb = rgb_led.RGBLED(17, 5, 16)
+sleep(0.001)
+# target_color = rgb.generate_random_color(True)
+rgb.set_color(255, 99, 0)
+# print(f'Your Target Color is {target_color}')
+
 
 dimmers = {}
 
